6312ce0dbc5ff92c20c40dedbf224fad818b27a4
Nguyen Hoang Anh <anh.nh@kyanon.digital>
2026-05-13 15:02:42 +0700

    feat: add IAddressRepository interface

--- src/DainnUser.Core/Interfaces/Repositories/IAddressRepository.cs
+++ src/DainnUser.Core/Interfaces/Repositories/IAddressRepository.cs
@@ -21,6 +21,29 @@ public interface IAddressRepository : IRepository<UserAddress>
     /// <param name="userId">The user identifier.</param>
     /// <param name="addressId">The address identifier.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <returns>The address if found and belongs to user, null otherwise.</returns>
-    Task<UserAddress?> GetByUserIdAndAddressIdAsync(Guid userId, Guid addressId, CancellationToken cancellationToken = default);
+    /// <returns>The address if found and belongs to user, otherwise null.</returns>
+    Task<UserAddress?> GetByUserIdAndIdAsync(Guid userId, Guid addressId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Clears the default flag for all addresses of a user.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    Task ClearDefaultForUserAsync(Guid userId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Gets the default address for a user.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>The default address if found, otherwise null.</returns>
+    Task<UserAddress?> GetDefaultForUserAsync(Guid userId, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Checks if a user has any addresses.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>True if user has addresses, otherwise false.</returns>
+    Task<bool> UserHasAddressesAsync(Guid userId, CancellationToken cancellationToken = default);
 }
